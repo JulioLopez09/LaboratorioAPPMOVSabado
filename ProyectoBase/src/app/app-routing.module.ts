cab26eff9d5a2f470915e1760ade237f5b6377af
Julio Lopez <jflg.09.98@gmail.com>
2019-09-16 14:42:24 -0500

    Login y registro

--- ProyectoBase/src/app/app-routing.module.ts
+++ ProyectoBase/src/app/app-routing.module.ts
@@ -7,7 +7,6 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   { path: 'registrar', loadChildren: './auth/registrar/registrar.module#RegistrarPageModule' },
-  { path: 'logic', loadChildren: './auth/logic/logic.module#LogicPageModule' },
   { path: 'login', loadChildren: './auth/login/login.module#LoginPageModule' }
 ];
 @NgModule({
