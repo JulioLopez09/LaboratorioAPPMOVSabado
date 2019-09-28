ff6c860c03c312a1c982f5e92fe78b10abe98f6c
Julio Lopez <jflg.09.98@gmail.com>
2019-09-28 09:00:53 -0500

    Practica Login

--- ProyectoBase/src/app/auth/registrar/registrar.module.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { RegistrarPage } from './registrar.page';
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [
   {
@@ -19,8 +20,11 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   declarations: [RegistrarPage]
 })
-export class RegistrarPageModule {}
+export class RegistrarPageModule { }
--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+//import { RegistrarService } from './registrar.service'
+//import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-registrar',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarPage implements OnInit {
 
-  constructor() { }
+  registrarForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {
+    this.registrarForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      correo: ['', Validators.required, Validators.email],
+      contrasena: ['', [Validators.required, Validators.minLength(6)]],
+      confirmacionContrasena: ['', Validators.required]
+    },
+    );
+  }
+
 
   ngOnInit() {
+  }
+
+  get f() { return this.registrarForm.controls; }
+
+  registrar() {
+    console.log(this.registrarForm.valid);
+  }
+
+  MustMatch(controlName: string, matchingControlName: string) {
+    return true;
   }
 
 }
