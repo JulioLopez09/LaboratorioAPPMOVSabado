085a09c48c79e9c0d9a5f3b9589437f4dd204ab5
Julio Lopez <jflg.09.98@gmail.com>
2019-10-12 08:56:51 -0500

    Mensaje en Login

--- ProyectoBase/src/app/model/usuario.ts
+++ ProyectoBase/src/app/model/usuario.ts
@@ -1,7 +1,7 @@
 import { HttpService } from 'src/app/service/http-service.service';
 
 export class Usuario {
-    public static endPoint = "/usuario";
+    public static endPoint = '/usuario';
     public nombre: string;
     public correo: string;
     public contrasena: string;
